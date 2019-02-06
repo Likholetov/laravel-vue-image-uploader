e77d76178b4f9ee5104af641233c4c50ee65a580
Alexandr Likholetov <likholetovalex@gmail.com>
2019-02-07 01:01:41 +0300

    image uploader

---
+++ resources/js/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Image Uploader</div>
+
+                    <div class="card-body">
+                        <image-uploader></image-uploader>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ImageUploader from "./ImageUploader"
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        components: {
+            ImageUploader
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import '~vue-toastr/src/vue-toastr.scss';
+</style>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content" id="app">
+                <app/>
+            </div>
+        </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
